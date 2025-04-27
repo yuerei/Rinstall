d7d961b6ef698d2169a90c7dfc941f341b906cec
Yue <63238154+yuerei@users.noreply.github.com>
2025-04-27 18:44:55 +0700

    Add tauri

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint']
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  devtools: { enabled: false },
+  ssr: false,
+  // Enables the development server to be discoverable by other devices when running on iOS physical devices
+  devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://v2.tauri.app/reference/environment-variables/
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+    },
+  }, 
 })
